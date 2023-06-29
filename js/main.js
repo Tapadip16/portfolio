6b8caa4d78f5789a985c4116eb096a26b905fb28
Tapadip16 <tapadippaul@gmail.com>
2023-06-29 10:22:51 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+import '../css/modern-normalize.css';
+import '../css/style.css';
+import '../css/components/header.css';
+import '../css/components/home.css';
+import '../css/components/about.css';
+import '../css/components/portfolio.css';
+import '../css/components/contact.css';
+import '../css/components/footer.css';
+import '../css/components/mobile-nav.css';
+import '../css/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+mobileNav();
+darkMode();
